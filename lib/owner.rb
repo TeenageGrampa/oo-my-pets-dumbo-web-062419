2b0dbf9b22d4aa0ae9caf79900f984d6a243138a
TeenageGrampa <danielhvoigt@gmail.com>
2019-07-01 11:58:34 -0400

    done

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,71 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_accessor 
+  attr_reader :species, :name
+  @@all = []
+  
+
+
+  def initialize(name)
+    @name = name
+    @species = 'human'
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+
+  def buy_cat(name)
+    cat = Cat.new(name, self)
+  end
+
+  def cats
+    Cat.all.select{|cat| cat.owner == self}
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name, self)
+  end
+
+  def dogs
+    Dog.all.select{|dog| dog.owner == self}
+  end
+
+  def walk_dogs
+    dogs.map{|dog| dog.mood = 'happy'}
+  end
+
+  def feed_cats
+    cats.map{|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    cats.map{|cat| cat.mood = 'nervous'}
+    cats.map{|cat| cat.owner = nil}
+    dogs.map{|dog| dog.mood = 'nervous'}
+    dogs.map{|dog| dog.owner = nil}
+    cats.clear
+    dogs.clear
+  end
+
+  def list_pets
+    "I have #{dogs.size} dog(s), and #{cats.size} cat(s)."
+  end
+
+
 end
+# binding.pry
